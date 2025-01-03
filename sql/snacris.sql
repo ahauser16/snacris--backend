86033b6aed75a3baafdceab47d64b811db14bf75
Arthur Hauser <arthur.hauser@gmail.com>
2025-01-03 12:53:55 -0500

    update seed files and utils function

--- sql/snacris.sql
+++ sql/snacris.sql
@@ -5,16 +5,17 @@ DROP DATABASE IF EXISTS snacris;
 CREATE DATABASE snacris;
 \connect snacris
 
-\i snacris-users-schema.sql
+\i schema/snacris-users-schema.sql
+\i schema/snacris-acris-code-mappings-schema.sql
 \i schema/snacris-acris-real-property-schema.sql
 \i schema/snacris-acris-personal-property-schema.sql
-\i schema/snacris-acris-code-mappings-schema.sql
 \i seed/snacris-seed-users.sql
 \i seed/snacris-seed-acris-document-control-codes.sql
 \i seed/snacris-seed-acris-ucc-collateral-type-codes.sql
 \i seed/snacris-seed-acris-property-type-codes.sql
 \i seed/snacris-seed-acris-usa-state-codes.sql
 \i seed/snacris-seed-acris-country-codes.sql
+\i seed/snacris-seed-acris-real-property.sql
 
 \echo 'Delete and recreate snacris_test db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
@@ -23,13 +24,14 @@ DROP DATABASE IF EXISTS snacris_test;
 CREATE DATABASE snacris_test;
 \connect snacris_test
 
-\i snacris-users-schema.sql
+\i schema/snacris-users-schema.sql
+\i schema/snacris-acris-code-mappings-schema.sql
 \i schema/snacris-acris-real-property-schema.sql
 \i schema/snacris-acris-personal-property-schema.sql
-\i schema/snacris-acris-code-mappings-schema.sql
 \i seed/snacris-seed-users.sql
 \i seed/snacris-seed-acris-document-control-codes.sql
 \i seed/snacris-seed-acris-ucc-collateral-type-codes.sql
 \i seed/snacris-seed-acris-property-type-codes.sql
 \i seed/snacris-seed-acris-usa-state-codes.sql
 \i seed/snacris-seed-acris-country-codes.sql
+\i seed/snacris-seed-acris-real-property.sql
--- sql/utils/convertJsonToSql.js
+++ sql/utils/convertJsonToSql.js
@@ -7,6 +7,8 @@
 // --> node convertJsonToSql.js acris-ucc-collateral-codes.json acris-ucc-collateral-codes-output.sql
 // --> node convertJsonToSql.js acris-usa-state-codes.json acris-usa-state-codes-output.sql
 // --> node convertJsonToSql.js acris-country-codes.json acris-country-codes-output.sql
+// --> node convertJsonToSql.js acris-real-property-legals.json acris-real-property-legals-output.sql
+// --> node convertJsonToSql.js acris-real-property-references.json acris-real-property-references-output.sql
 // *
 // *
 
@@ -75,6 +77,36 @@ function convertCountryCodesToSql(jsonArray) {
   return sql;
 }
 
+// Function to convert ACRIS Real Property Legals JSON to SQL
+function convertRealPropertyLegalsDataToSql(jsonArray) {
+  let sql = "INSERT INTO acris_real_property_legals (document_id, record_type, borough, block, lot, easement, partial_lot, air_rights, subterranean_rights, property_type, street_number, street_name, good_through_date)\nVALUES\n";
+
+  const values = jsonArray.map(item => {
+    const streetNumber = item.street_number ? `'${item.street_number}'` : 'NULL';
+    const streetName = item.street_name ? `'${item.street_name}'` : 'NULL';
+
+    return `('${item.document_id}', '${item.record_type}', ${item.borough}, ${item.block}, ${item.lot}, '${item.easement}', '${item.partial_lot}', '${item.air_rights}', '${item.subterranean_rights}', '${item.property_type}', ${streetNumber}, ${streetName}, '${item.good_through_date}')`;
+  });
+
+  sql += values.join(',\n') + ';';
+  return sql;
+}
+
+// Function to convert ACRIS Real Property References JSON to SQL
+function convertRealPropertyReferencesDataToSql(jsonArray) {
+  let sql = "INSERT INTO acris_real_property_references (document_id, record_type, reference_by_crfn_, reference_by_doc_id, reference_by_reel_year, reference_by_reel_borough, reference_by_reel_nbr, reference_by_reel_page, good_through_date)\nVALUES\n";
+
+  const values = jsonArray.map(item => {
+    const referenceByCrfn = item.reference_by_crfn_ ? `'${item.reference_by_crfn_}'` : 'NULL';
+    const referenceByDocId = item.reference_by_doc_id ? `'${item.reference_by_doc_id}'` : 'NULL';
+
+    return `('${item.document_id}', '${item.record_type}', ${referenceByCrfn}, ${referenceByDocId}, ${item.reference_by_reel_year}, ${item.reference_by_reel_borough}, ${item.reference_by_reel_nbr}, ${item.reference_by_reel_page}, '${item.good_through_date}')`;
+  });
+
+  sql += values.join(',\n') + ';';
+  return sql;
+}
+
 // Main function to handle the conversion based on the input file
 function convertJsonToSql(inputFile, outputFile) {
   const jsonData = JSON.parse(fs.readFileSync(inputFile, 'utf8'));
@@ -90,6 +122,10 @@ function convertJsonToSql(inputFile, outputFile) {
     sqlData = convertStateCodesToSql(jsonData);
   } else if (inputFile.includes('acris-country-codes')) {
     sqlData = convertCountryCodesToSql(jsonData);
+  } else if (inputFile.includes('acris-real-property-legals')) {
+    sqlData = convertRealPropertyLegalsDataToSql(jsonData);
+  } else if (inputFile.includes('acris-real-property-references')) {
+    sqlData = convertRealPropertyReferencesDataToSql(jsonData);
   } else {
     throw new Error('Unsupported JSON file format');
   }
